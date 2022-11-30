d78ea3c90107964dac994a0de96410e9068bfe9b
JeffreyWu <pa022069@gmail.com>
2022-12-01 01:56:18 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  // swcMinify: true,
   env: {
     apiKey: 'api-key',
   },
-  presets: ["next/babel"]
 }
 
 module.exports = nextConfig
